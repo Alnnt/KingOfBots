459ceab43cf367a4678ad27f195e30e86da0cae2
Alnnt <1138745158@qq.com>
2023-06-02 05:26:33 +0800

    complete bot CRUD controller.

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -1,33 +1,52 @@
 <template>
-<ContentField>我的Bot</ContentField>
+  <div class="container">
+    <div class="row">
+      <div class="col-3">
+        <div class="card">
+          <div class="card-body">
+            <img :src="$store.state.user.photo" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="col-9">
+        <div class="card">
+          <div class="card-header">
+            <span class="title">我的Bot</span>
+            <button type="button" class="btn btn-primary float-end">创建Bot</button>
+          </div>
+          <div class="card-body">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import ContentField from '@/components/ContentField.vue'
-import $ from 'jquery'
-import { useStore } from "vuex";
+import { ref } from "vue"
+import $ from "jquery"
+import { useStore } from "vuex"
 
 const store = useStore();
-
-$.ajax({
-  url: "http://localhost:8090/user/bot/remove",
-  type: "post",
-  data: {
-    bot_id: 2
-
-  },
-  headers: {
-    Authorization: "Bearer " + store.state.user.token
-  },
-  success(resp) {
-    console.log(resp);
-  },
-  error(resp) {
-    console.log(resp);
-  }
-})
+const refresh_bots = () => {
+  $.ajax({
+    url: "http://localhost:8090/user/bot/getlist",
+    type: "get"
+  })
+}
 </script>
 
 <style scoped>
+.card {
+  margin-top: 20px;
+}
 
+img {
+  width: 100%
+}
+
+.title {
+  font-size: 130%;
+}
 </style>
